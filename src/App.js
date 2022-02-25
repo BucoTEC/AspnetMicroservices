b97ac62bb116b4a1026d911c24c5fd3a06901462
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-25 23:06:23 +0100

    setup apolo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
 
 
 function App() {
   return (
+    <ApolloProvider client={client}>
+
     <div >
       <h1>test</h1>
+      <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 
