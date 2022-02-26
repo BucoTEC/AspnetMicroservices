a02a64eb3e34e2937ca8055bc572e794e13971d6
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-26 10:13:38 +0100

    fixed value filtering

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {getAuthors} from '../queries/queries'
 import {
     useQuery
@@ -8,24 +8,31 @@ import {
 
 export default function AddBook() {
     const { loading, error, data } = useQuery(getAuthors);
-    console.log(data);
+    const [name, setName]=useState('')
+    const [genre, setGenre]=useState('')
+    const [authorId, setAuthorId]=useState('')
+            console.log(authorId);
+    const formHandler = (e)=>{
+        e.preventDefault()
+    }
+
   return (
-    <form id="add-book"  >
+    <form id="add-book" onSubmit={formHandler} >
     <div className="field">
         <label>Book name:</label>
-        <input type="text"  />
+        <input type="text" onChange={e => setName(e.target.value)} />
     </div>
     <div className="field">
         <label>Genre:</label>
-        <input type="text"  />
+        <input type="text" onChange={e => setGenre(e.target.value)} />
     </div>
     <div className="field">
         <label>Author:</label>
-        <select  >
+        <select onChange={e=> setAuthorId(e.target.value)}>
             <option>Select author</option>
             { loading && <option>Loading ...</option> }
             { error && <option>Ups there was a problem</option> }
-            { data &&  data.authors.map(author => <option key={author.id}>{author.name}</option> )  }
+            { data &&  data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option> )  }
 
         </select>
     </div>
