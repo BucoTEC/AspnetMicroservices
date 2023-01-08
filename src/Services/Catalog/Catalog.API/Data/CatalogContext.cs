4317b4b00cee0ae7f775f520b2072af301cc9982
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-01-08 19:31:52 +0100

    add basic mogno seed

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -9,6 +9,19 @@ namespace Catalog.API.Data
 {
     public class CatalogContext : ICatalogContext
     {
-        public IMongoCollection<Product> Product => throw new NotImplementedException();
+        private readonly IConfiguration _configuration;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var db = client.GetDatabase(_configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = db.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+
+        }
+
+
+        public IMongoCollection<Product> Products { get; }
     }
 }
