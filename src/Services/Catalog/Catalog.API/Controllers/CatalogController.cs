d6ac8df65ed6e7c528e70e435bb9d3c2c6f5c932
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-01-14 21:30:32 +0100

    fix controller

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -30,6 +30,7 @@ namespace Catalog.API.Controllers
             return Ok(products);
         }
 
+        // basic setup of id req
         [HttpGet("{id:length(24)}", Name = "GetProduct")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
