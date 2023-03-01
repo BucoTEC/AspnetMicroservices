b760f2b365a48933d27fab6748aeaf7a5138a143
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-01 13:48:38 +0100

    add basic discount repository

--- src/Services/Discount/Discount.API/Entities/Coupon.cs
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -11,7 +11,7 @@ namespace Discount.API.Entities
 
         public string ProductName { get; set; } = null!;
 
-        public string ProductDescription { get; set; } = null!;
+        public string Description { get; set; } = null!;
 
         public int Amount { get; set; }
     }
