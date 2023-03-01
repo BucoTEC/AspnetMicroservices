b6e0a2ad421a275b85f063b9cbad3c21e1970130
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-01 13:39:09 +0100

    add basic entities

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public string ProductDescription { get; set; } = null!;
+
+        public int Amount { get; set; }
+    }
+}
