e8372e1cc94229f1bdc9fb645c5f78763ffebb75
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-05 00:05:08 +0100

    add entotoes to grpc

---
+++ src/Services/Discount/Discount.Grpc/Entities/Coupon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int Amount { get; set; }
+    }
+}
