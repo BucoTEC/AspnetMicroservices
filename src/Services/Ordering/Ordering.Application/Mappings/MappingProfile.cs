08e5d7b937de104e9336e8707172c986ca6e3bd5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-11 11:13:06 +0100

    setup basic mapper

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
-        
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            // CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            // CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
     }
 }
