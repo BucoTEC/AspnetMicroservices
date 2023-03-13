de672c9b9f0848efd1946ae9a1b0414f6371d00e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-13 18:54:32 +0100

    test version

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// TODO test git
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
