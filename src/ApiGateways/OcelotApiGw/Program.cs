cfb7fa88d4b92c0e9a1ce2ade9791f0a4d1120a2
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-03-24 15:21:36 +0100

    update routing definitions

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -4,6 +4,8 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment}.json", true, true);
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
